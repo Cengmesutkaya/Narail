fd559ed72719bd6253c7b474478e8093c387006c
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2018-08-16 13:29:03 +0300

    Admin Panel Projeye Eklendi.

---
+++ Narail/Controllers/BlogAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Narail.Controllers
+{
+    public class BlogAdminController : Controller
+    {
+        // GET: BlogAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Narail/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Narail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Narail.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        NarailDBEntities db = new NarailDBEntities();
+        public ActionResult Index()
+        {
+            var model = db.Author.ToList();
+            return View();
+        }
+    }
+}
